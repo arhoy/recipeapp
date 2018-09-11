97e4dc23a05d47f408b76fab5ab1bd0c62bfc250
aquasar <arhoy@ualberta.ca>
2018-09-10 20:15:16 -0600

    footer made yes

--- js/views/searchView.js
+++ js/views/searchView.js
@@ -20,7 +20,6 @@ const renderRecipe = (recipe)=>{
                 <h4 class="recipe__header">${recipe.title}</h4>
             </div>
             <button type="button" href = "#${recipe.recipe_id}" class="btn recipe__button" data-toggle="modal" data-id = "${recipe.recipe_id}" data-target="#recipe">Show Ingredients</button>
-
             <div class="modal fade" id="recipe" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
                     <div class="modal-dialog modal-dialog-centered" role="document">
                         <div class="modal-content">
@@ -29,11 +28,10 @@ const renderRecipe = (recipe)=>{
                     </div>
             </div><!--modal end-->
     </div>
-
-
-    
+  
     `;
     elements.results.insertAdjacentHTML('beforeend',html);
+
 
 }
 
@@ -45,6 +43,28 @@ export const clearResults = ()=>{
 export const printResults = (recipes)=>{
 
    recipes.forEach(renderRecipe); 
+   // inser footer at the end.
+
+   const html__footer = `
+   <footer class = "footer">
+            <div class="footer__icons">
+                    <div class="footer__icon">
+                        <i class="fab fa-twitter"></i>
+                    </div>
+                    <div class="footer__icon">
+                        <i class="fab fa-linkedin-in"></i>
+                    </div>
+                    <div class="footer__icon">
+                        <i class="fab fa-github-square"></i>
+                    </div>
+            </div>
+            <div class="footer__signature">
+                Created by <a class = "footer__link" href="https://dash-intel.com/">dash-intel.com</a>
+            </div>      
+    </footer>   
+    
+   `;
+   elements.results.insertAdjacentHTML('afterend',html__footer);
  
 }
 
